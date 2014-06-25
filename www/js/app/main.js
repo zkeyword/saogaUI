f5d1d55a2f038ee570b570c0d64adbeadc8897a8
norion <zjx1247225@163.com>
2014-06-25 09:09:26 +0800

    'fixed'

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,12 +1,14 @@
 define(function(require){
-	var saogaUI  = require('core/saogaUI'),
-		template = require('template'),
-		drag     = require('core/drag'),
-		dialog   = require('core/dialog');
+	var saogaUI   = require('core/saogaUI'),
+		template  = require('template'),
+		drag      = require('core/drag'),
+		dialog    = require('core/dialog'),
+		validator = require('core/validator')
 	
 	saogaUI.ui.drag   = drag;
 	saogaUI.ui.dialog = dialog;
 	saogaUI.template  = template;
+	saogaUI.ui.validator = validator;
 	
 	return saogaUI;
 });
--- www/js/core/validator.js
+++ www/js/core/validator.js
@@ -1,5 +1,17 @@
 define(function(){
 	
+	'use strict';
 	
+	var validator = function(options){
+		var o = options || {};
+		var init = function(){
+			console.log(options)
+		};
+		
+		return {
+			init: init
+		}
+	};
 	
+	return validator;
 });
