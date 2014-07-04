d18a739fe06028afea78c38a567c4b84719835fe
norion <zjx1247225@163.com>
2014-07-04 18:06:14 +0800

    fixed saogaUI

--- www/js/core/dorpDownTree.js
+++ www/js/core/dorpDownTree.js
@@ -5,12 +5,13 @@ define(['core/saogaUI'], function(saogaUI){
 	var dorpDownTree = function(options){
 		var o = options || {};
 		if(!o.target){return false;}
-		var g          = this,
-			data       = o.data,
-			target     = $(o.target),
-			height     = o.height ? o.height : 'auto',
-			selectedID = o.selected,
-			isMultiple = o.isMultiple || false;
+		var g           = this,
+			data        = o.data,
+			target      = $(o.target),
+			height      = o.height ? o.height : 'auto',
+			selectedID  = o.selected,
+			isMultiple  = o.isMultiple || false,
+			onclickItem = o.onclickItem;
 		
 		var _core = {
 				
@@ -63,8 +64,8 @@ define(['core/saogaUI'], function(saogaUI){
 						'data-name': name,
 						'data-value': val
 					});
-					if( fn && saogaUI.base.isFunction(fn) ){
-						fn(name, val);
+					if( onclickItem && saogaUI.base.isFunction(onclickItem) ){
+						onclickItem(name, val);
 					}
 				});
 			},
@@ -72,76 +73,26 @@ define(['core/saogaUI'], function(saogaUI){
 			/**
 			* 创建树对象
 			*/
-			init: function(){
+			run: function(){
 				_core.createHtml(data);
+				_core.onItem();
 			}
 		};
 		
 		g.init = function(){
-			_core.init(data);
+			_core.run(data);
+			return g;
 		};
 		
-		var init = function(fn){
-				target.html( createTreeHtml(data) );
-				target.find('.l-treeItem').on('click',function(){
-					var that = $(this),
-						name = that.text(),
-						val  = that.attr('data-value');
-					target.find('.l-tree').attr({
-						'data-name': name,
-						'data-value': val
-					});
-					if( fn && saogaUI.base.isFunction(fn) ){
-						fn(name, val);
-					}
-				});
-			},
-			refresh = function(data){
-				target.html( createTreeHtml(data) );
-			},
-			createTreeHtml = function(data){
-				var line = '',
-					node = '<div class="l-treeNode"></div>',
-					tree = function(data, line, isOrigin){
-						var i        = 0,
-							len      = data.length,
-							html     = '',
-							selected;
+		g.refresh = function(){
+			return g;
+		};
+		
 
-						for(; i<len; i++){
-							html += '<div class="l-treeItemWrap fn-clear'+ (selectedID == data[i].val ? ' l-treeSelected' : '') +'">';
-								if( isOrigin ){
-									html += '<div class="l-treeBox l-treeExpandable-open"></div>';
-								}else{
-									html += '<div class="l-treeBox l-treeLine"></div>';
-								}
-								html += (isMultiple ? '<div class="l-treeCheckbox"></div>' : '');
-								
-								//替换字符串，考虑修改
-								html +=	(line ? line + node : '');
-								html = html.replace('"></div><div class="l-treeNode"></div>', ' l-treeNode"></div>');
-								
-								html += '<div class="l-treeItem" data-value='+ data[i].val +'>'+ data[i].name +'</div>';
-							
-							html += '</div>';
-									
-							if( data[i].chlidren ){
-								html += '<div class="l-treeChildrenWrap fn-clear">'+ tree(data[i].chlidren, line+'<div class="l-treeBox l-treeLine"></div>', false) +'</div>';
-							}
-						}
-											
-						return html;
-					};
-				
-				return '<div class="l-tree">'+ tree(data, line, true) +'</div>';
-			};
-		
-		
-		return {
-			init: init,
-			refresh: refresh
-		};
 	};
 	
-	return dorpDownTree;
+	return function(options){
+		var tree = new dorpDownTree(options);
+		return tree.init();
+	};
 });
